4290eb92110a02b8255b2ba1f193a608aefd8ad4
Martin Ang <46913612+martinang2@users.noreply.github.com>
2025-10-03 15:16:36 +1000

    Add react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import QueryProvider from "@/lib/providers/query-provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <QueryProvider>
+          {children}
+        </QueryProvider>
+
       </body>
     </html>
   );
