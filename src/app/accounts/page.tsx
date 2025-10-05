921f98c420ff23932f071856899ba81a621c31af
Martin Ang <46913612+martinang2@users.noreply.github.com>
2025-10-05 19:09:52 +1100

    Added Simple routing

---
+++ src/app/accounts/page.tsx
@@ -0,0 +1,5 @@
+const AccountsPage = () => {
+    return <div className="p-4">Accounts Page</div>;
+};
+
+export default AccountsPage;
---
+++ src/app/payments/page.tsx
@@ -0,0 +1,5 @@
+const PaymentsPage = () => {
+    return <div className="p-4">Payments Page</div>;
+};
+
+export default PaymentsPage;
